b8006fc7cf2ca13787a5dc161144cd7dcda5092f
Mattabulous <49161819+Mattabulous@users.noreply.github.com>
2020-11-02 19:16:58 +0000

    Initial project files

---
+++ Assets/PickUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform destination;
+
+    public void PickUpObject(Transform dest)
+    {
+        destination = dest;
+
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        GetComponent<Collider>().enabled = false;
+        transform.position = dest.position;
+        transform.rotation = dest.rotation;
+
+        transform.parent = dest;
+    }
+
+    public void DropObject()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Collider>().enabled = true;
+
+        transform.parent = null;
+    }
+}
---
+++ Assets/PlayerInteraction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] Transform pickUpPoint;
+    private bool objectGrabbed;
+    private PickUp objectPickUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 5))
+        {
+            if(hit.collider.GetComponent<PickUp>())
+            {
+                if(Input.GetKeyDown(KeyCode.E) && !objectGrabbed)
+                {
+                    objectPickUp = hit.collider.GetComponent<PickUp>();
+                    objectPickUp.PickUpObject(pickUpPoint);
+                    objectGrabbed = true;
+                }
+            }
+        }
+
+        if(objectGrabbed)
+        {
+            if(Input.GetKeyUp(KeyCode.E))
+            {
+                objectPickUp.DropObject();
+                objectGrabbed = false;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Debug.DrawRay(cam.transform.position, cam.transform.forward * 5, Color.red);
+    }
+}
